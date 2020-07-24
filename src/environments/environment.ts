1e469606f986394d5eabc8169afac125d34de64f
unknown <bscoutinho2012@gmail.com>
2020-07-24 16:09:29 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "suApiKey",
+    authDomain: "dominioGenerado",
+    databaseURL: "urlBaseDatos",
+    projectId: "proyectoID",
+    storageBucket: "urlImagenes",
+    messagingSenderId: "mensajesID",
+    appId: "suAppID"
+  }
+};
+
+//Esto lo encontraran en clase Agregar AngularFire2 del proyecto final
